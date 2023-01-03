67f396037e81f8174ba17d02bc87c32274f88688
Геннадий Коротков <gennadijkorotkov@MacBook-Air-Gennadij.local>
2023-01-03 20:14:27 +0500

    homework2

---
+++ react-deploy/src/shared/ConcatLine.tsx
@@ -0,0 +1,65 @@
+import { hot } from "react-hot-loader/root";
+import * as React from "react";
+import styles from "./header.css";
+
+interface ContactLineProps {
+    a: string,
+    b: string
+  }
+  export function ConcatLine(props: ContactLineProps) {
+  const concat = () => {
+    return props.a + props.b;
+  }
+    return (
+      <header>
+        <h1 className={styles.example}>{concat()}</h1>
+      </header>
+    );
+  }
+
+type withDataType = {
+    howIDoIt: string,
+    simeArray: Array<string | number>,
+}
+
+interface MySecondInterface {
+    howIDoIt: string,
+    simeArray: Array<string | number>,
+    withData: Array<withDataType>,
+}
+    
+  const MyHometask: MySecondInterface = {
+    howIDoIt: "I Do It Wel",
+    simeArray: ["string one", "string two", 42],
+     withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23] }],
+}
+const myArray: MyArray<number> = [1,2,3];
+
+interface MyArray<T> {
+    [N: number]: T;
+    reduce(callbackfn: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T, initialValue: T): T;
+}
+
+const initialValue = 0;
+myArray.reduce((accumulator, value) => accumulator + value, initialValue); 
+console.log(myArray.reduce((accumulator, value) => accumulator + value, initialValue));
+
+
+interface IHomeTask {
+    data: string;
+    numbericData: number;
+    date: Date;
+    externalData: {
+        basis: number;
+        value: string;
+    }
+}
+const homeTask:  MyPartial<IHomeTask> = {
+    externalData: {
+        value: 'win'
+    }
+}
+type MyPartial<T> = {
+    [N in keyof Partial<T>]: T[N] extends object ? MyPartial<T[N]> : T[N];
+}
+console.log(homeTask)
--- react-deploy/src/shared/Header.tsx
+++ react-deploy/src/shared/Header.tsx
@@ -1,13 +1,16 @@
 import { hot } from "react-hot-loader/root";
 import * as React from "react";
 import styles from "./header.css";
+import { ConcatLine } from "./ConcatLine";
 
 function HeaderComponent() {
   return (
     <header>
       <h1 className={styles.example}>Hslo Rseaact(hello)</h1>
+      <ConcatLine a={'1'} b={'2'}/>
     </header>
   );
 }
 
 export const Header = hot(HeaderComponent);
+
